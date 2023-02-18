03c8406a4e25ec435dadadf5dfe881607db71e06
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-02-19 02:55:17 +0900

    State Pattern

---
+++ StatePattern/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace StatePattern
+{
+    public interface State
+    {
+        public void Excute();
+    }
+
+    public class Attack : State
+    {
+        public void Excute()
+        {
+            System.Console.WriteLine("공격");
+        }
+    }
+
+    public class Jump : State
+    {
+        public void Excute()
+        {
+            System.Console.WriteLine("점프");
+        }
+    }
+
+    public class Jeonglee
+    {
+        private State state;
+
+        public Jeonglee(State state)
+        {
+            this.state = state;
+        }
+
+        public void Update()
+        {
+            state.Excute();
+        }
+    }
+
+    class Program
+    {
+        public void Main(string[] args)
+        {
+            
+        }
+    }
+    
+}
