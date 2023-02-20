bcb67c89fc2440a4c2c1ee38dc0dd10f0de5f0ea
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-02-21 01:13:12 +0900

    CshapLinq

---
+++ CshapLINQ/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace CshapeLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            // 2. Query creation.
+            // numQuery is an IEnumerable<int>
+            var numQuery =
+                from num in numbers
+                where (num % 2) == 0
+                select num;
+
+            // 3. Query execution.
+            foreach (int num in numQuery)
+            {
+                Console.Write("{0,1} ", num);
+            }
+            IQueryable<int> a;
+        }
+    }
+
+
+// Create a data source by using a collection initializer.
+
+}
