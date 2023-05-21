e575009f7667a3ac585da0c98feef81d8ccdb5d3
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-05-21 21:25:04 +0900

    Docs: Organizing folders

---
+++ ProgrammingPatterns/BuilderPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BuilderPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextOptions textOptions = GetDefaultTextOptions();
+            
+            System.Console.WriteLine(textOptions.GetFont);
+            System.Console.WriteLine(textOptions.GetFontSize);
+        }
+
+        static TextOptions GetDefaultTextOptions()
+        {
+            return new TextOptionsBuilder("ARIAL")
+            .SetFontSize(12.0f)
+            .bulid();
+        }
+    }
+}
