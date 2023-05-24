76997d602e854a9e768ce103e6884dfbe7509c76
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-05-25 00:14:39 +0900

    Review: C# CallBack Delegate

---
+++ C#Programming/C#CallBackFuncAndDelegate/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    delegate bool delegateCheck(int n);
+
+    static void Main(string[] args)
+    {
+        List<int> numbers = Enumerable.Range(1, 200).ToList();
+
+        var oddNumbers = numbers.Find(n => n % 2 == 1);
+
+        var test = numbers.TrueForAll(n => n < 50);
+
+        numbers.RemoveAll(n => n % 2 == 0);
+
+        numbers.ForEach(item => Console.WriteLine(item));
+    }
+}
