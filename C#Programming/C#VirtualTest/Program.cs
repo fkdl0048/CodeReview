cfdffbfe919ec4defe4afd84335703d048e318e9
Jeongan Lee <84510455+fkdl0048@users.noreply.github.com>
2023-08-31 01:44:23 +0900

    Review: Effective Item 16

---
+++ C#Programming/C#VirtualTest/Program.cs
@@ -0,0 +1,29 @@
+﻿abstract class B
+{
+    protected B()
+    {
+        VFunc();
+    }
+
+    protected abstract void VFunc();
+}
+
+class Derived : B
+{
+    private readonly string msg = "Set by initializer";
+
+    public Derived(string mag)
+    {
+        this.msg = msg;
+    }
+
+    protected override void VFunc()
+    {
+        Console.WriteLine(msg);
+    }
+
+    public static void Main()
+    {
+        var d = new Derived("Set by constructor");
+    }
+}
